d22b5282c2b22052d38da695e6ff2be8c02fe325
SamHummerstone <sjhummerstone@gmail.com>
2018-11-27 23:32:59 +0000

    Dockfile fix and initial run.sh

---
+++ gmod/run.sh
@@ -0,0 +1,2 @@
+# docker run -it --rm  -p 27015:27015 -p 27005:27005 --name=gmod sjhummerstone/gmod-server:latest ./gmodserver start --host 127.0.0.1 --user gmod
+docker run -it --rm  -p 27015:27015 -p 27005:27005 --name=gmod sjhummerstone/gmod-server:latest /bin/bash
