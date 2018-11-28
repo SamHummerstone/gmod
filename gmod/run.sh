b7c56f8b6c24f0129caff3d1483c73e20b72085e
SamHummerstone <sjhummerstone@gmail.com>
2018-11-28 20:08:58 +0000

    Begin debugging config locations

--- gmod/run.sh
+++ gmod/run.sh
@@ -1,2 +1,2 @@
 # docker run -it --rm  -p 27015:27015 -p 27005:27005 --name=gmod sjhummerstone/gmod-server:latest ./gmodserver start --host 127.0.0.1 --user gmod
-docker run -it --rm  -p 27015:27015 -p 27005:27005 --name=gmod sjhummerstone/gmod-server:latest /bin/bash
+docker run -it --network=host --rm  -p 0.0.0.0:27015:27015 -p 0.0.0.0:27005:27005 --name=gmod sjhummerstone/gmod-server:latest /bin/bash
